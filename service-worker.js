54d678d2e854d61fdbc39aed107ef012e5e58a01
swd <stphndcky22@gmail.com>
2022-06-21 12:19:51 -0700

    created activation event listener for service worker, created fetch event listener to retrieve info from cache offline

--- service-worker.js
+++ service-worker.js
@@ -40,6 +40,62 @@ self.addEventListener('install', function (e) {
     )
 });
 
+// we must create an eventListener for activation of
+// the service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        // keys() returns array of cache names
+        // we name the array keyList
+        caches.keys().then(function (keyList) {
+            // we store our filtered caches in cacheKeeplist array
+            let cacheKeeplist = keyList.filter(function (key) {
+                // we are interested in caches that have the prefix
+                // for our app, which is FoodFest
+                return key.indexOf(APP_PREFIX);
+            });
+            // we push the cache to the array
+            cacheKeeplist.push(CACHE_NAME);
+
+            // if cache is not in keyList it will get value of -1
+            // these caches will be deleted
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+
+// we must add an eventListener for retrieving info from cache
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url)
+    // respondWith method will deliver resource
+    e.respondWith(
+        // we check cache to see if our requested 
+        // resource is stored
+        caches.match(e.request).then(function(request) {
+            // if requested resource is in cache
+            // it is returned to us
+            if(request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            }
+            // if requested resource is not in cache
+            // then we fetch it via online network
+            else {
+                console.log('file not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
+
 
 // remember we will be able to use service worker
 // with https or localhost 8080
